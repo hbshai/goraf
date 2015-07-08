a7bb1467844dba39caa5c672fb86597387adcc6e
Axel Smeets <murlocbrand+git@gmail.com>
2015-07-08 13:00:33 +0200

    Initial commit

---
+++ programedit.go
@@ -0,0 +1,173 @@
+package main
+
+import (
+    "fmt"
+    "net/http"
+    "flag"
+    "encoding/json"
+    "os"
+    "time"
+    "io"
+    "io/ioutil"
+    "errors"
+)
+
+var (
+    serverPort = flag.String("port", "127.0.0.1:8000", "server address (host:port)")
+    backupDirPath = flag.String("backup", "./backup", "path to backup directory")
+    filePath = flag.String("file", "./programs.json", "path to programs.json")
+    
+    lastAccess = time.Now()
+    lastSessionAddress = "no one"
+    protectionTime = time.Second * 20
+)
+
+type appError struct {
+    Error error
+    Message string
+    Code int
+}
+
+type Program struct {
+    Name string `json:"name"`
+    RSS string `json:"rss"`
+    Image string `json:"image"`
+    Description string `json:"description"`
+    Category string `json:"category"`
+}
+
+type appHandler func(http.ResponseWriter, *http.Request) *appError
+
+func (fn appHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+    if err := fn(w, r); err != nil {
+        fmt.Printf("%s: %v\n", err.Message, err.Error)
+        http.Error(w, err.Message, err.Code)
+    }
+}
+
+func accessProtected(r *http.Request) bool {
+    // We are the active user
+    if r.RemoteAddr == lastSessionAddress {
+        return false
+    }
+
+    return time.Since(lastAccess) < protectionTime
+}
+func giveAccess(r *http.Request) {
+    lastSessionAddress = r.RemoteAddr
+    lastAccess = time.Now()
+}
+
+// https://gist.github.com/elazarl/5507969
+func cp(d *os.File, src string) error {
+    s, err := os.Open(src)
+    if err != nil {
+        return err
+    }
+    // no need to check errors on read only file, we already got everything
+    // we need from the filesystem, so nothing can go wrong now.
+    defer s.Close()
+    if _, err := io.Copy(d, s); err != nil {
+        d.Close()
+        return err
+    }
+    return d.Close()
+}
+
+func makeBackup() *appError {
+    // Try to make a backup for safety purposes
+    f, err := ioutil.TempFile(*backupDirPath, "programs.json-")
+    if err != nil {
+        return &appError{err, "Couldn't create backup file", 0}
+    }
+
+    if err = cp(f, *filePath); err != nil {
+        return &appError{err, "Failed to copy source to backup", 0}
+    }
+        
+    return nil
+}
+
+func handlePrograms(w http.ResponseWriter, r *http.Request) *appError {
+    if accessProtected(r) {
+        dur := fmt.Sprintf("%d", int((protectionTime - time.Since(lastAccess)).Seconds()))
+        return &appError{errors.New("Conflicting access"), dur, 400}
+    }
+    giveAccess(r)
+
+    if r.Method == "GET" {
+        data, err := ioutil.ReadFile(*filePath)
+        if err != nil {
+            return &appError{err, "Coudln't find/read the programs.json file :(", 500}
+        }
+
+        w.Write(data)
+        return nil
+    }
+
+    if r.Method == "POST" {
+        if err := r.ParseForm(); err != nil {
+            return &appError{err, "Error: Couldn't parse form data, check log and contact the IT monkey", 400}
+        }
+        
+        keys := r.Form["programs[][key]"]
+        names := r.Form["programs[][name]"]
+        rss := r.Form["programs[][rss]"]
+        images := r.Form["programs[][image]"]
+        categories := r.Form["programs[][category]"]
+        descriptions := r.Form["programs[][description]"]
+
+        programs := make(map[string]Program)
+        for i := 0; i < len(keys); i++ {
+            programs[keys[i]] = Program{
+                Name: names[i],
+                RSS: rss[i],
+                Image: images[i],
+                Category: categories[i],
+                Description: descriptions[i],
+            }
+        }
+
+        // Output nicely formatted JSON
+        data, err := json.MarshalIndent(programs, "", "   ")
+        if err != nil {
+            return &appError{err, "Error: Couldn't convert data to json, check log and contact IT turtle", 500}
+        }
+        
+        backupErr := makeBackup()
+        if backupErr != nil {
+            fmt.Printf("%s: %v\n", backupErr.Message, backupErr.Error)
+        }
+        
+        if err = ioutil.WriteFile(*filePath, data, 0644); err != nil {
+            return &appError{err, "Couldn't write to programs.json, perhaps some premission error?", 500}
+        }
+
+        fmt.Printf("Wrote %d bytes\n", len(data))
+        if backupErr == nil {
+            fmt.Fprintf(w, "Success: %d programs saved (~%d kB)", len(programs), len(data)/1000)
+        } else {
+            fmt.Fprintf(w, "Warning: %d programs saved (~%d kB), but failed to backup.", len(programs), len(data)/1000)
+        }    
+    }
+
+    return nil
+}
+
+func main() {
+    flag.Parse()
+    mux := http.NewServeMux()
+
+    if _, err := os.Stat(*backupDirPath); os.IsNotExist(err) {
+        fmt.Println("Creating backup directory in " + *backupDirPath + "...")
+        os.Mkdir(*backupDirPath, 0644)
+    }
+    
+    fs := http.FileServer(http.Dir("./public"))
+    mux.Handle("/", http.StripPrefix("/", fs))
+    
+    mux.Handle("/programs", appHandler(handlePrograms))
+
+    fmt.Printf("Running http server on address %s\n", *serverPort)
+    http.ListenAndServe(*serverPort, mux)
+}
---
+++ public/js/main.js
@@ -0,0 +1,220 @@
+(function(){
+
+    function formatError(time) {
+        var t = ""
+        
+        if (time > 60)
+            t = Math.round(time/60) + ' min'
+        else
+            t = time + ' sec'
+
+        return "Another person recently changed something. To prevent overwriting each other's data please wait until he/she is done. If no additional changes are made you will gain access in " + t + "."
+
+    }
+    
+    function displayError(txt) {
+        // +1 for good measure
+        var time = parseInt(txt) + 1
+
+        sweetAlert("Edit Conflict", formatError(time), "error");
+        setTimeout(function countdown() {
+            var el = document.querySelector(".sweet-alert > p")
+            el.textContent = formatError(--time)
+            if (time > 0)
+                setTimeout(countdown, 1000)
+            else
+                window.location.reload(true)        
+        }, 1000)
+    }
+
+    // Classic XHR to get programs.json
+    function fetchPrograms (callb) {
+        var xhr = new XMLHttpRequest()
+
+        xhr.open('GET', '/programs')
+
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState === 4) {
+                if (xhr.status !== 200)
+                    return displayError(xhr.responseText)
+
+                callb(JSON.parse(xhr.responseText))
+            }
+        }
+        xhr.send()
+    }
+
+    // Shorthand to make the input elements
+    function input(name) {
+        var node = document.createElement('input')
+        
+        node.setAttribute('name', name)
+        node.setAttribute('autocomplete', 'off')
+        node.setAttribute('type', 'text')
+
+        return node
+    }
+    function textarea(name) {
+        var node = document.createElement('textarea')
+        
+        node.setAttribute('name', name)
+        node.setAttribute('autocomplete', 'off')
+        node.setAttribute('form', 'programs')
+
+        return node
+    }
+
+    function label(name){
+        var node = document.createElement('label')
+        node.textContent = name
+        return node
+    }
+
+    // Generate one program of the form:
+    // key { name: "", rss : "", image : "", description : ""}
+    function generateProgram(key, program) {
+        var div = document.createElement('div'),
+            row = document.createElement('div'),
+            cont = document.createElement('div'),
+            programKey = input('programs[][key]'),
+            programName = input('programs[][name]'),
+            programRSS = input('programs[][rss]'),
+            programCategory = input('programs[][category]'),
+            programInfo = textarea('programs[][description]'),
+            programImg = input('programs[][image]'),
+            btnEdit = document.createElement('button'),
+            btnDel = document.createElement('button')
+
+        btnEdit.textContent = 'edit'
+        btnEdit.onclick = function () {
+            // Because button is inside form we must prevent submit
+            event.preventDefault()
+
+            // Animate using max height. Should be set to something
+            // larger than container will ever be, but not too large
+            // because the animation will animate over all pixels
+            if (cont.style.maxHeight === '500px') {
+                cont.style.maxHeight = 0
+                cont.style.transition = ''
+            } else {
+                cont.style.maxHeight = '500px'
+                cont.style.transition = 'max-height 0.2s ease-in'
+            }
+
+            var dont = cont.clientHeight
+        }
+
+        btnDel.textContent = 'delete'
+        btnDel.onclick = function() {
+            // Because button is inside form we must prevent submit
+            event.preventDefault()
+
+            sweetAlert({
+                title: "Are you sure?",
+                text: "You need to manually recover '" + key + "' once deleted.",
+                type: "warning",
+                showCancelButton: true,
+                confirmButtonColor: "#DD6B55",
+                confirmButtonText: "Yes, delete it!",
+                closeOnConfirm: false,
+                html: false
+            }, function(){
+                swal("Deleted!",
+                    "Program was deleted for you. Click 'save' to update server as well. Refresh this web page if you misclicked.",
+                    "success");
+                div.parentNode.removeChild(div)
+            });
+        }
+        
+        programKey.value = key
+        programName.value = program.name
+        programRSS.value = program.rss
+        programImg.value = program.image
+        programCategory.value = program.category
+        programInfo.value = program.description
+        
+        cont.appendChild(label('Name:'))
+        cont.appendChild(programName)
+
+        cont.appendChild(label('Category:'))
+        cont.appendChild(programCategory)
+
+        cont.appendChild(label('Description:'))
+        cont.appendChild(programInfo)
+
+        cont.appendChild(label('RSS:'))
+        cont.appendChild(programRSS)
+
+        cont.appendChild(label('Image:'))
+        cont.appendChild(programImg)
+        cont.classList.add('program-container')
+
+        row.appendChild(programKey)
+        row.appendChild(btnEdit)
+        row.appendChild(btnDel)
+        row.classList.add('program-header')
+
+        div.appendChild(row)
+        div.appendChild(cont)
+        div.classList.add('program')
+
+        return div
+    }
+
+    function generatePrograms(programs){
+        var keys = Object.keys(programs),
+            parent = document.getElementById('programs')
+
+        keys
+            .map(function (k) {
+                return generateProgram(k, programs[k])
+            })
+            .forEach(function (div) {
+                parent.appendChild(div)
+            })
+    }
+
+    function flashPostResults(frame){
+        var flashes = 3
+
+        var white = function(){
+            frame.style.background = '#AAFFAA'
+            if (flashes-- > 0)
+                setTimeout(blue, 100)
+        }
+        
+        var blue = function(){
+            frame.style.background = '#E0EBF5'
+            if (flashes-- > 0)
+                setTimeout(white, 100)
+        }
+
+        white()
+    }
+
+    function newProgram(event){
+        // Because button is inside form we must prevent submit
+        event.preventDefault()
+
+        var parent = document.getElementById('programs'),
+            first = parent.childNodes[2],
+            boiler = generateProgram('FIXME: program key', {
+                name : 'FIXME: program name',
+                rss : 'FIXME: program rss link',
+                image : 'FIXME: program image link'
+            })
+        parent.insertBefore(boiler, first)
+    }
+
+    window.onload = function () {
+        fetchPrograms(generatePrograms)
+
+        var res = document.querySelector('iframe')
+        res.onload = function(){
+            flashPostResults(res)
+        }
+
+        document.getElementById('btn-add').onclick = newProgram
+    }
+
+})();
